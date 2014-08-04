1ea084317acbd7f90dae3be71faf3719f04331a9
Jesse Allison <jesse@electrotap.com>
2014-08-04 00:49:47 -0500

    Working UIImagePicker

--- InstaGrab/MAGViewController.h
+++ InstaGrab/MAGViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MAGViewController : UIViewController
+@interface MAGViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
--- InstaGrab/MAGViewController.m
+++ InstaGrab/MAGViewController.m
@@ -9,6 +9,7 @@
 #import "MAGViewController.h"
 
 @interface MAGViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *pictureDisplay;
 
 @end
 
@@ -18,12 +19,35 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self pictureDisplay].image = [UIImage imageNamed:@"InstaGrab.png"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if([@"choosePhoto" isEqualToString:segue.identifier])
+    {
+        UIImagePickerController *controller = [[segue destinationViewController] init];
+        controller.delegate = self;
+        controller.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *pic = [info valueForKey:UIImagePickerControllerOriginalImage];
+    [self pictureDisplay].image = pic;
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:NULL];
 }
 
 @end
